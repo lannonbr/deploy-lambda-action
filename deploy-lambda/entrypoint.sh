ecd103874dedcd1bec47c7d976cd9db2030c4bfb
Benjamin Lannon <benjamin@lannonbr.com>
2018-12-01 15:25:16 -0500

    Adding deploy-lambda action

---
+++ deploy-lambda/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+message=$(jq -r .issue.body "$GITHUB_EVENT_PATH")
+echo "$message" | grep -Eq "!deploy" || {
+  echo "Didn't contain !deploy. neutral exit..."
+  exit 78
+}
+
+npm i
+
+echo "LS:"
+ls .
+
+zip -r lambda.zip *
+
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} \
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} \
+AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION} aws \
+  lambda update-function-code \
+  --function-name "${AWS_LAMBDA_NAME}" \
+  --zip-file fileb://lambda.zip
